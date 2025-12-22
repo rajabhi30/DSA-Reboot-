515fb245d6fd6c708e8069aad40616c9e2483334
Abhishek kumar <rajabhishek8070@gmail.com>
2025-12-22 23:55:20 +0530

    Map

---
+++ STL/map.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    // unordered_map<int,int>mp;
+
+    // mp[1]=100;
+    // mp[2]=200;
+    // mp[3]=300;
+
+    // unordered_map<int,int>::iterator it=mp.begin();
+
+    // while(it!=mp.end()){
+    //     pair<int,int> val=*it;
+    //     cout<<val.first<<" "<<val.second<<endl;
+    //     it++;
+    // }
+    // cout<<endl;
+    // if(mp.find(2)!=mp.end()){
+    //     cout<<"element is present";
+    // }
+    // else{
+    //     cout<<"element is not present";
+    // }
+
+    map<int,int>mp;
+
+    mp[1]=100;
+    mp[2]=200;
+    mp[3]=300;
+
+    map<int,int>::iterator it=mp.begin();
+
+    while(it!=mp.end()){
+        pair<int,int> val=*it;
+        cout<<val.first<<" "<<val.second<<endl;
+        it++;
+    }
+    cout<<endl;
+    if(mp.find(2)!=mp.end()){
+        cout<<"element is present";
+    }
+    else{
+        cout<<"element is not present";
+    }
+
+    return 0;
+}
